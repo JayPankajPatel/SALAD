6e671e9ea2b3a4fa01093c5a179bce3348305da5
JayPankajPatel <jayp102501@gmail.com>
2023-06-04 12:59:41 -0700

    Added scheduler

---
+++ src/scheduler.py
@@ -0,0 +1,33 @@
+"""
+Schedule jobs at specified times or intervals.
+
+TODO Create a configuration file for this module.
+"""
+import time
+from datetime import datetime
+
+import schedule
+
+from camera import USBCamera
+
+
+def timelaspe() -> None:
+    """
+    Take pictures with intent of making a timelaspe.
+
+    :return None
+    :rtype None
+    """
+    camera_obj = USBCamera(0)
+    file_name = f"{datetime.now}.jpg"
+    print(f"Taking picture called: {file_name}")
+    camera_obj.take_and_save("../pictures", file_name)
+
+
+if __name__ == "__main__":
+    PICTURE_INTERVAL = 20
+    schedule.every(PICTURE_INTERVAL).seconds.do(timelaspe)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
