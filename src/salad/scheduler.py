402db5b44f8484f2e7c5ad6199dbb24bd61ece43
JayPankajPatel <jayp102501@gmail.com>
2023-06-16 23:48:02 -0700

    (#5) Removed old code from schedule.py

--- src/salad/scheduler.py
+++ src/salad/scheduler.py
@@ -22,24 +22,6 @@ relays = {
 stack_one = relay.BoardStack(0, relays)
 webcam = camera.USBCamera(0)
 
-# PICTURE_DIR_NAME = "pictures"
-
-
-# def timelaspe() -> None:
-#     """
-#     Take pictures with intent of making a timelaspe.
-#
-#     :return None
-#     :rtype None
-#     """
-#     camera_obj = USBCamera(0)
-#
-#     create_directory_in_script_location(PICTURE_DIR_NAME)
-#     current_datetime = datetime.now().strftime("%d-%m-%y-%H:%M:%S")
-#     image_file_name = f"{current_datetime}.jpg"
-#     print(f"Taking picture called: {image_file_name}")
-#     camera_obj.take_and_save("../pictures", image_file_name)
-#
 app = Rocketry()
 
 relays = {
